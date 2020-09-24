57beb5045cd67e4d573de3e34aed33bd483b68a7
ganirockz <42479860+ganirockz@users.noreply.github.com>
2020-09-24 15:08:13 +0530

    Modified useCase4

--- src/com/capgemini/SnakeAndLadder.java
+++ src/com/capgemini/SnakeAndLadder.java
@@ -5,7 +5,7 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and Ladder");
 		int position = 0;
-		while(position <= 100) {
+		while(position < 100) {
 		int number_on_dice = (int) Math.floor(Math.random()*10)%6 +1;
 		System.out.println("The number on the dice is: "+number_on_dice);
 		int option = (int) Math.floor(Math.random()*10)%3;
